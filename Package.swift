4728e772afb5e958dadaeafb3a872c4171c18317
Terrance Wood <terrance.james.wood@gmail.com>
2024-03-21 15:14:32 -0700

    Build 1.11.0+OpenSSL_1_1_1w

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "CSSH",
                       url: "https://github.com/TheTekton337/Libssh2Prebuild/releases/download/1.11.0+OpenSSL_1_1_1w/CSSH-1.11.0+OpenSSL_1_1_1w.xcframework.zip",
-                      checksum: "238cd19276f02c0a3e96e1164580472c68ea0466ab5bf32298b30347d6999ed8")
+                      checksum: "39f95567a09653280bd68f330a478076071a03fdc40785ef88b1bf035cb815cf")
     ]
 )
